9441dc336335dc3121173e4ccce5d3bebb408a86
nabilahann <13519097@std.stei.itb.ac.id>
2022-05-26 15:23:25 +0700

    fix joblist

--- src/components/JobList.js
+++ src/components/JobList.js
@@ -1,58 +1,66 @@
-import React, { useState, useRef, useCallback } from 'react';
-import useJobSearch from './hook/useJobSearch';
+import React, { useState, useEffect } from 'react';
 import JobHolder from './JobHolder';
+import axios from 'axios';
 
 function JobList() {
-    const [pageNumber, setPageNumber] = useState(1)
+    const [pageNumber, setPageNumber] = useState(1);
+    const [loading, setLoading] = useState(true)
+    const [error, setError] = useState(false)
+    const [jobs, setJobs] = useState([])
+    const [hasMore, setHasMore] = useState(false)
 
-    const {
-        jobs,
-        hasMore,
-        loading,
-        error
-    } = useJobSearch(pageNumber)
-
-    const observer = useRef()
-    const lastJobsElementRef = useCallback(node => {
-        if (loading) return
-        if (observer.current) observer.current.disconnect()
-        observer.current = new IntersectionObserver(entries => {
-        if (entries[0].isIntersecting && hasMore) {
-            setPageNumber(prevPageNumber => prevPageNumber + 1)
-        }
+    useEffect(() => {
+        setLoading(true)
+        setError(false)
+        let cancel
+        axios({
+          method: 'GET',
+          url: `http://dev3.dansmultipro.co.id/api/recruitment/positions.json?page=${pageNumber}`,
+          cancelToken: new axios.CancelToken(c => cancel = c)
+        }).then(res => {
+            setJobs(prevJobs => {
+            return [...new Set([...prevJobs, ...res.data.map((job) => job
+            )])]
+          })
+          if(pageNumber < 2) {
+            setHasMore(true)
+          } else {
+            setHasMore(false)
+          }
+        //   setHasMore(res.data.length > 0)
+          setLoading(false)
+        }).catch(e => {
+          if (axios.isCancel(e)) return
+          setError(true)
         })
-        if (node) observer.current.observe(node)
-    }, [loading, hasMore])
+        return () => cancel()
+      }, [pageNumber])
+
+    function MoreJobs() {
+        setPageNumber(pageNumber + 1);
+    }
 
     return (
         <>
         <h3 className="font-bold text-xl mb-3">Job List</h3>
-        
         {jobs.map((job, index) => {
             if(job != null){
-                if (jobs.length === index + 1) {
-                    return (
-                        <div ref={lastJobsElementRef} key={job.id} className="flex-no-shrink w-full md:w-1/4 md:px-3">
-                            <JobHolder
-                                job={job}
-                            />
-                        </div>
-                    )
-                } else {
-                    return (
-                        <div key={job.id} className="flex-no-shrink w-full md:w-1/4 md:px-3">
-                            <JobHolder
-                                job={job}
-                            />
-                        </div>
-                    )
-                }
-            }
-            
+                return (
+                    <div key={job.id} className="flex-no-shrink w-full md:w-1/4 md:px-3">
+                        <JobHolder
+                            job={job}
+                        />
+                    </div>
+                )
+                
+            } 
         })}
-        <div>{loading && 'Loading...' }</div>
+
+        <div className="loading">{loading && 'Loading...' }</div>
         <div>{error && 'Error'}</div>
+        <div>{hasMore && <button type="button" className="more-btn"  onClick={() => MoreJobs()}>More Jobs</button>}</div>
         </>
+        
     )
 
 }
